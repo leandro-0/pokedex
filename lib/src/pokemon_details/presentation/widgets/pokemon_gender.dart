9651d4bc705141b57a36ba1693de3adc7f4b75db
Steven <smmr0001@ce.pucmm.edu.do>
2024-11-04 19:12:20 -0400

    feat: creating pokemon gender widget

---
+++ lib/src/pokemon_details/presentation/widgets/pokemon_gender.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class PokemonGender extends StatelessWidget {
+  final double malePercentage;
+  final double femalePercentage;
+
+  const PokemonGender({
+    super.key,
+    required this.malePercentage,
+    required this.femalePercentage,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        const Text(
+          'Gender: ',
+          style: TextStyle(color: Colors.grey, fontSize: 16),
+        ),
+        const SizedBox(width: 10),
+        Row(
+          children: [
+            Icon(FontAwesomeIcons.mars, color: Colors.blue, size: 16),
+            const SizedBox(width: 4),
+            Text(
+              '${malePercentage.toStringAsFixed(1)}%',
+              style: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 16),
+            ),
+          ],
+        ),
+        const SizedBox(width: 12),
+        Row(
+          children: [
+            Icon(FontAwesomeIcons.venus, color: Colors.pink, size: 16),
+            const SizedBox(width: 4),
+            Text(
+              '${femalePercentage.toStringAsFixed(1)}%',
+              style: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 16),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}

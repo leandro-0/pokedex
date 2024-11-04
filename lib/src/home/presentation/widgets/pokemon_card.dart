c08bf5ddef35d1b769600b274225dd3e08e9624d
Leandro Jiménez <leandroj21p@gmail.com>
2024-11-04 13:45:25 -0400

    feat(pokemon-card): add pokeball background

--- lib/src/home/presentation/widgets/pokemon_card.dart
+++ lib/src/home/presentation/widgets/pokemon_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:pokedex/core/theme/app_theme.dart';
 import 'package:pokedex/src/home/data/models/pokemon_tile.dart';
 import 'package:pokedex/src/pokemon_details/presentation/views/pokemon_details.dart';
@@ -25,8 +26,8 @@ class PokemonCard extends StatelessWidget {
         ),
         child: Container(
           padding: const EdgeInsets.symmetric(
-            vertical: 8.0,
-            horizontal: 15.0,
+            horizontal: 8.0,
+            vertical: 5.0,
           ),
           decoration: BoxDecoration(
             color: AppTheme.typeColors[pk.types.first],
@@ -34,13 +35,30 @@ class PokemonCard extends StatelessWidget {
           ),
           child: Row(
             children: [
-              Hero(
-                tag: 'card-${pk.id}',
-                child: Image.network(
-                  pk.spriteUrl,
-                  width: imgSize,
-                  height: imgSize,
-                ),
+              Stack(
+                children: [
+                  SvgPicture.asset(
+                    'assets/icons/pokeball_background.svg',
+                    width: imgSize + 10.0,
+                    height: imgSize + 10.0,
+                    colorFilter: ColorFilter.mode(
+                      Colors.white.withOpacity(0.2),
+                      BlendMode.srcIn,
+                    ),
+                  ),
+                  Positioned(
+                    left: 7.0,
+                    top: 7.0,
+                    child: Hero(
+                      tag: 'card-${pk.id}',
+                      child: Image.network(
+                        pk.spriteUrl,
+                        width: imgSize,
+                        height: imgSize,
+                      ),
+                    ),
+                  ),
+                ],
               ),
               const SizedBox(width: 15.0),
               Column(

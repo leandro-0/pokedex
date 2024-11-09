4f1180c42ee9f7e49dfb56b730ab1c348f2d0c53
Steven <smmr0001@ce.pucmm.edu.do>
2024-11-08 22:02:28 -0400

    feat(details): implement dynamic fetching of Pokémon stats with animation

--- lib/src/pokemon_details/presentation/widgets/pokemon_stats.dart
+++ lib/src/pokemon_details/presentation/widgets/pokemon_stats.dart
@@ -1,59 +1,108 @@
 import 'package:flutter/material.dart';
+import 'package:pokedex/src/pokemon_details/presentation/widgets/stat_item.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:pokedex/src/pokemon_details/data/repository/details_repository.dart';
 
-class PokemonStats extends StatelessWidget {
-  final List<Map<String, dynamic>> stats;
-  final List<String> weaknesses;
+// pokemon_stats.dart
+class PokemonStats extends StatefulWidget {
+  final int id;
 
   const PokemonStats({
     super.key,
-    required this.stats,
-    required this.weaknesses,
+    required this.id,
   });
 
   @override
+  State<PokemonStats> createState() => _PokemonStatsState();
+}
+
+class _PokemonStatsState extends State<PokemonStats>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 800),
+      vsync: this,
+    );
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    );
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  final List<String> orderedStatLabels = [
+    'Hp',
+    'Attack',
+    'Defense',
+    'Sp. Atk',
+    'Sp. Def',
+    'Speed'
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Base Stats',
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 10),
-          ...stats.map((stat) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(stat['name'].toString().toUpperCase()),
-                  Text(stat['base_stat'].toString()),
-                ],
+    final client = GraphQLProvider.of(context).value;
+
+    return FutureBuilder<List<Map<String, dynamic>>>(
+      future: DetailsRepository.getPokemonStats(client, widget.id),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        }
+
+        final stats = snapshot.data!;
+        final total =
+            stats.fold<int>(0, (sum, stat) => sum + (stat['base_stat'] as int));
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Base Stats',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
-            );
-          }).toList(),
-          SizedBox(height: 20),
-          Text(
-            'Weaknesses',
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 10),
-          Wrap(
-            spacing: 8,
-            children: weaknesses.map((weakness) {
-              return Chip(
-                label: Text(
-                  weakness.toUpperCase(),
-                  style: TextStyle(color: Colors.white),
+              const SizedBox(height: 16),
+              ...List.generate(stats.length, (index) => 
+                  StatItem(
+                  animation: _animation,
+                  label: orderedStatLabels[index],
+                  value: stats[index]['base_stat'] as int,
+                  color: _getStatColor(stats[index]['base_stat'] as int),
                 ),
-                backgroundColor: Colors.redAccent,
-              );
-            }).toList(),
+              ),
+              StatItem(
+                animation: _animation,
+                label: 'Total',
+                value: total,
+                color: _getStatColor(total ~/ stats.length),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
+
+  Color _getStatColor(int value) {
+    if (value < 50) return Colors.red;
+    if (value < 70) return Colors.orange;
+    return Colors.teal;
+  }
+
 }

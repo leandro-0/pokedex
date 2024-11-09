fdfc52062a1bb0db75b08f9c5f98ec91382af385
Steven <smmr0001@ce.pucmm.edu.do>
2024-11-08 22:02:38 -0400

    feat(details): add StatItem widget for displaying Pokémon stats with animation

---
+++ lib/src/pokemon_details/presentation/widgets/stat_item.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex/src/pokemon_details/presentation/widgets/progress_bar.dart';	
+
+final Map<String, String> statLabels = {
+  'hp': 'Hp',
+  'attack': 'Attack',
+  'defense': 'Defense',
+  'special-attack': 'Sp. Atk',
+  'special-defense': 'Sp. Def',
+  'speed': 'Speed',
+};
+
+class StatItem extends StatelessWidget {
+  final Animation<double> animation;
+  final String label;
+  final int value;
+  final double? progress;
+  final Color color;
+
+  const StatItem({
+    super.key,
+    required this.animation,
+    required this.label,
+    required this.value,
+    required this.color,
+    this.progress,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Text(
+              statLabels[label] ?? label,
+              style: const TextStyle(
+                fontSize: 14,
+                color: Colors.grey,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 40,
+            child: Text(
+              value.toString(),
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: AnimatedBuilder(
+              animation: animation,
+              builder: (context, _) {
+                return ProgressBar(
+                  progress: animation.value * (progress ?? value / 100),
+                  color: color,
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

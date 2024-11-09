9d6d79f66aa0c00e2b7f2e2fe5180dde3f321f73
Steven <smmr0001@ce.pucmm.edu.do>
2024-11-09 12:00:55 -0400

    feat(rounded_text_field): update RoundedTextField to require controller and hintText, add onChanged callback

--- lib/src/home/presentation/widgets/rounded_text_field.dart
+++ lib/src/home/presentation/widgets/rounded_text_field.dart
@@ -1,28 +1,33 @@
 import 'package:flutter/material.dart';
 
 class RoundedTextField extends StatelessWidget {
-  final TextEditingController? controller;
-  final double borderRadius;
-  final String? hintText;
+  final TextEditingController controller;
+  final String hintText;
   final Widget? prefixIcon;
+  final ValueChanged<String>? onChanged;
 
   const RoundedTextField({
     super.key,
-    this.borderRadius = 10.0,
-    this.hintText,
+    required this.controller,
+    required this.hintText,
     this.prefixIcon,
-    this.controller,
+    this.onChanged,
   });
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       controller: controller,
+      onChanged: onChanged,
       decoration: InputDecoration(
         hintText: hintText,
         prefixIcon: prefixIcon,
         border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(borderRadius),
+          borderRadius: BorderRadius.circular(30),
+        ),
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 15,
         ),
       ),
     );

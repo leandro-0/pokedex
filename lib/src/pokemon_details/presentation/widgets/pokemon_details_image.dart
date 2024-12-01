b395b55c18f5d83024c10df27b19f675462653c1
Leandro Jiménez <leandroj21p@gmail.com>
2024-11-30 21:59:23 -0400

    refactor: favorites

--- lib/src/pokemon_details/presentation/widgets/pokemon_details_image.dart
+++ lib/src/pokemon_details/presentation/widgets/pokemon_details_image.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:pokedex/core/utils/utils.dart';
-import 'package:pokedex/src/favorite_pokemons/data/repository/favorite_pokemon_repository.dart';
 import 'package:pokedex/src/home/data/models/pokemon_tile.dart';
 import 'package:pokedex/src/home/presentation/widgets/pokemon_types.dart';
 import 'package:pokedex/src/pokemon_details/data/repository/details_repository.dart';
+import 'package:pokedex/src/pokemon_details/presentation/widgets/add_favorite_button.dart';
 
 class PokemonDetailsImage extends StatefulWidget {
   final PokemonTile pkBasicInfo;
@@ -135,85 +135,6 @@ class _PokemonDetailsImageState extends State<PokemonDetailsImage> {
                 ),
               );
             },
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class AddFavoriteButton extends StatefulWidget {
-  const AddFavoriteButton({super.key, required this.pkBasicInfo});
-
-  final PokemonTile pkBasicInfo;
-
-  @override
-  State<AddFavoriteButton> createState() => _AddFavoriteButtonState();
-}
-
-class _AddFavoriteButtonState extends State<AddFavoriteButton> {
-  int? _favoriteId;
-  bool? _isFavorite;
-
-  void _loadInitialData() async {
-    final favoriteId =
-        await FavoritePokemonRepository.isFavorite(widget.pkBasicInfo.id);
-    setState(() {
-      if (!context.mounted) return;
-      _favoriteId = favoriteId;
-      _isFavorite = favoriteId != null;
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _loadInitialData();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (_favoriteId == null && _isFavorite == null) return const SizedBox();
-
-    return Container(
-      width: double.infinity,
-      height: double.infinity,
-      color: Colors.transparent,
-      child: Stack(
-        children: [
-          Align(
-            alignment: const Alignment(0.97, -0.7),
-            child: IconButton(
-              icon: AnimatedSwitcher(
-                duration: const Duration(milliseconds: 300),
-                transitionBuilder: (child, animation) {
-                  return ScaleTransition(scale: animation, child: child);
-                },
-                child: Icon(
-                  _isFavorite! ? Icons.favorite : Icons.favorite_border,
-                  key: ValueKey<bool>(_isFavorite!),
-                  color: _isFavorite!
-                      ? Colors.red[300]!.withOpacity(0.9)
-                      : Colors.white,
-                ),
-              ),
-              onPressed: () async {
-                setState(() => _isFavorite = !_isFavorite!);
-
-                if (!_isFavorite!) {
-                  if (_favoriteId == null) return;
-                  await FavoritePokemonRepository.removeFavorite(_favoriteId!);
-                  _favoriteId = null;
-                } else {
-                  _favoriteId = await FavoritePokemonRepository.addFavorite(
-                    widget.pkBasicInfo,
-                  );
-                  if (_favoriteId == null) _isFavorite = false;
-                }
-                if (!context.mounted) return;
-                setState(() {});
-              },
-            ),
           ),
         ],
       ),
